5b1b272833a37419243677a6acc06fad2a718ee9
Dave Fishel <dave.fishel@gmail.com>
2023-11-07 20:28:29 -0800

    fix bugs, update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# hexconverter
+# Hex Converter
+
+A simple app for converting between hex and decimal written in C++/raylib/raygui.
+
+![Screenshot of the app converting 0xFACE to -1330 as int16_t](./assets/screenshot.png)
+
+I built this app to assist with developing the NES emulator that I'm working on. I end up often debugging memory addresses and since I'm bad at converting these on the fly, this saves me a bunch of time. Maybe you'll find it useful as well.
+
+## How to Build
+
+The easiest way is to clone the repo and run `./build.sh && ./run.sh`. This is verified to work on Mac and Windows under a bash shell such as MINGW. You can also open this project in VSCode and if you have the CMake extension installed, it will likely prompt you to select a kit, and you can build/debug using VSCode as your IDE. I've tested this flow on Mac and Windows, using Clang 14.x on Mac and VS Community 2022 on Windows, and both seem to build and run just fine.
--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,17 @@ const std::unordered_map<char, int> charToDecDigit{
     {'0', 0}, {'1', 1}, {'2', 2}, {'3', 3}, {'4', 4}, {'5', 5}, {'6', 6}, {'7', 7}, {'8', 8}, {'9', 9},
 };
 
+enum {
+  DEC_MODE_U8 = 0,
+  DEC_MODE_S8,
+  DEC_MODE_U16,
+  DEC_MODE_S16,
+  DEC_MODE_U32,
+  DEC_MODE_S32,
+  DEC_MODE_U64,
+  DEC_MODE_S64,
+};
+
 std::string hexStrToBinaryStr(const std::string hexStr) {
   if (hexStr.empty()) {
     return 0;
@@ -45,6 +56,35 @@ std::string hexStrToDecimalStr(const std::string hexStr) {
     decimal += hexToDecimal.at(*iter) * pow16;
   }
   return std::to_string(decimal);
+}
+
+std::string hexStrToDecimalStrForMode(const std::string hexStr, const int mode) {
+  switch (mode) {
+    case DEC_MODE_U8:
+      return hexStrToDecimalStr<uint8_t>(hexStr);
+      break;
+    case DEC_MODE_S8:
+      return hexStrToDecimalStr<int8_t>(hexStr);
+      break;
+    case DEC_MODE_U16:
+      return hexStrToDecimalStr<uint16_t>(hexStr);
+      break;
+    case DEC_MODE_S16:
+      return hexStrToDecimalStr<int16_t>(hexStr);
+      break;
+    case DEC_MODE_U32:
+      return hexStrToDecimalStr<uint32_t>(hexStr);
+      break;
+    case DEC_MODE_S32:
+      return hexStrToDecimalStr<int32_t>(hexStr);
+      break;
+    case DEC_MODE_U64:
+      return hexStrToDecimalStr<uint64_t>(hexStr);
+      break;
+    case DEC_MODE_S64:
+      return hexStrToDecimalStr<int64_t>(hexStr);
+      break;
+  }
 }
 
 template <typename IntType>
@@ -75,38 +115,75 @@ std::string decStrToHexStr(std::string decStr) {
   }
 
   if (std::is_signed<IntType>::value) {
-    SignedIntType signedDec = -1 * static_cast<SignedIntType>(decimal);
-    outputStream << signedDec;
+    SignedIntType signedDec = (negative ? -1 : 1) * static_cast<SignedIntType>(decimal);
+    if (sizeof(IntType) == 1) {
+      // Special case for bytes: make sure they are written out as numbers rather than chars
+      outputStream << static_cast<uint16_t>(static_cast<int8_t>(signedDec) & 0x00FF);
+    } else {
+      outputStream << signedDec;
+    }
   } else {
-    outputStream << decimal;
+    if (sizeof(IntType) == 1) {
+      // Special case for bytes: make sure they are written out as numbers rather than chars
+      outputStream << static_cast<uint16_t>(decimal);
+    } else {
+      outputStream << decimal;
+    }
   }
   return outputStream.str();
 }
 
-template <typename IntType>
-std::string decStrToBinaryStr(const std::string decStr) {
-  return hexStrToBinaryStr(decStrToHexStr<IntType>(decStr));
+std::string decStrToHexStrForMode(const std::string decStr, const int mode) {
+  switch (mode) {
+    case DEC_MODE_U8:
+      return decStrToHexStr<uint8_t>(decStr);
+      break;
+    case DEC_MODE_S8:
+      return decStrToHexStr<int8_t>(decStr);
+      break;
+    case DEC_MODE_U16:
+      return decStrToHexStr<uint16_t>(decStr);
+      break;
+    case DEC_MODE_S16:
+      return decStrToHexStr<int16_t>(decStr);
+      break;
+    case DEC_MODE_U32:
+      return decStrToHexStr<uint32_t>(decStr);
+      break;
+    case DEC_MODE_S32:
+      return decStrToHexStr<int32_t>(decStr);
+      break;
+    case DEC_MODE_U64:
+      return decStrToHexStr<uint64_t>(decStr);
+      break;
+    case DEC_MODE_S64:
+      return decStrToHexStr<int64_t>(decStr);
+      break;
+  }
 }
+
+std::string decStrToBinaryStr(const std::string decStr) { return hexStrToBinaryStr(decStrToHexStr<uint64_t>(decStr)); }
 
 std::string hexLabelStr{"Hex"};
 constexpr size_t hexBoxTextLen = 9;
 char hexBoxTextRaw[hexBoxTextLen] = "";
-std::string hexBoxTextCleaned = "";
 bool hexBoxEditMode = true;
 
 std::string decLabelStr{"Decimal"};
 constexpr size_t decBoxTextLen = 17;
 char decBoxTextRaw[decBoxTextLen] = "";
-std::string decBoxTextCleaned = "";
 bool decBoxEditMode = false;
 
 std::string binLabelStr{"Binary"};
 constexpr size_t binBoxTextLen = 65;
 char binBoxTextRaw[binBoxTextLen] = "";
-std::string binBoxTextCleaned = "";
 bool binBoxEditMode = false;
 
-int toggleGroup = 0;
+int decMode = DEC_MODE_S8;
+bool decModeToggled = false;
+bool hexInputChanged = false;
+bool decInputChanged = false;
+bool binInputChanged = false;
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -119,6 +196,8 @@ int main(void) {
 
   InitWindow(screenWidth, screenHeight, "Hex Converter");
   GuiSetIconScale(2);
+
+  SetTargetFPS(360);
   //--------------------------------------------------------------------------------------
 
   // Main game loop
@@ -143,76 +222,131 @@ int main(void) {
         if (clipboard.starts_with("0x")) {
           clipboard.erase(0, 2);
         }
-        strncpy_s(hexBoxTextRaw, clipboard.c_str(), clipboard.length());
+        strncpy(hexBoxTextRaw, clipboard.c_str(), clipboard.length());
       } else if (decBoxEditMode && clipboard.length() <= decBoxTextLen - 1) {
-        strncpy_s(decBoxTextRaw, clipboard.c_str(), clipboard.length());
+        strncpy(decBoxTextRaw, clipboard.c_str(), clipboard.length());
       } else if (binBoxEditMode && clipboard.length() <= binBoxTextLen - 1) {
-        strncpy_s(binBoxTextRaw, clipboard.c_str(), clipboard.length());
+        strncpy(binBoxTextRaw, clipboard.c_str(), clipboard.length());
       }
     }
 
-    std::string hexStrRaw{hexBoxTextRaw};
-    if (!hexStrRaw.empty() && hexStrRaw != hexBoxTextCleaned) {
-      // Something has been typed into the hex input, convert it to uppercase then filter out any bad characters
-      std::transform(hexStrRaw.begin(), hexStrRaw.end(), hexStrRaw.begin(), std::toupper);
-      hexStrRaw.erase(
-          std::remove_if(hexStrRaw.begin(), hexStrRaw.end(), [](auto const& c) { return !hexChars.contains(c); }));
-      // Update the value of the text boxes to reflect the cleaned value
-      memset(hexBoxTextRaw, 0, hexBoxTextLen);
-      hexBoxTextCleaned = hexStrRaw;
-      strncpy_s(hexBoxTextRaw, hexBoxTextCleaned.c_str(), hexBoxTextCleaned.size());
-      // Parse this string to an integer and convert it to decimal and binary
-      std::string decimalStr = hexStrToDecimalStr<uint32_t>(hexBoxTextCleaned);
-      memset(decBoxTextRaw, 0, decBoxTextLen);
-      strncpy_s(decBoxTextRaw, decimalStr.c_str(), decimalStr.size());
-      std::string binaryStr = hexStrToBinaryStr(hexBoxTextCleaned);
-      memset(binBoxTextRaw, 0, binBoxTextLen);
-      strncpy_s(binBoxTextRaw, binaryStr.c_str(), binaryStr.size());
+    if (decModeToggled) {
+      decModeToggled = false;
+      std::string hexStrRaw{hexBoxTextRaw};
+      if (!hexStrRaw.empty()) {
+        // On changing decimal mode, we reinterpret the hex input as the appropriate type
+        std::string decimalStr = hexStrToDecimalStrForMode(std::string{hexStrRaw}, decMode);
+        memset(decBoxTextRaw, 0, decBoxTextLen);
+        strncpy(decBoxTextRaw, decimalStr.c_str(), decimalStr.size());
+      }
     }
 
-    std::string decStrRaw{decBoxTextRaw};
-    bool hasOnlyMinus = (decStrRaw.size() == 1) && (decStrRaw.at(0) == '-');
-    if (!decStrRaw.empty() && !hasOnlyMinus && decStrRaw != decBoxTextCleaned) {
-      // Something has been typed into the decimal input, filter out bad characters
-      auto removeStart = decStrRaw.begin();
-      bool hasNegativeSign = false;
-      bool needToReplaceCleanedWithMinus = false;
-      if (!decStrRaw.empty() && !decChars.contains(decStrRaw.at(0))) {
-        // Front of string doesn't have a number, remove it unless its a minus sign
-        if (!(decStrRaw.at(0) == '-')) {
-          decStrRaw.erase(0, 1);
-        } else {
-          hasNegativeSign = true;
-          ++removeStart;
+    if (hexInputChanged) {
+      std::string hexStrRaw{hexBoxTextRaw};
+      if (!hexStrRaw.empty()) {
+        // Something has been typed into the hex input, convert it to uppercase then filter out any bad characters
+        std::for_each(hexStrRaw.begin(), hexStrRaw.end(), [](char& c) { c = std::toupper(c); });
+        hexStrRaw.erase(
+            std::remove_if(hexStrRaw.begin(), hexStrRaw.end(), [](auto const& c) { return !hexChars.contains(c); }));
+        // Update the value of the text boxes to reflect the cleaned value
+        memset(hexBoxTextRaw, 0, hexBoxTextLen);
+        if (!hexStrRaw.empty()) {
+          strncpy(hexBoxTextRaw, hexStrRaw.c_str(), hexStrRaw.size());
         }
-      }
-      decStrRaw.erase(
-          std::remove_if(removeStart, decStrRaw.end(), [](auto const& c) { return !decChars.contains(c); }));
-      // Update the value of the text boxes to reflect the cleaned value
-      memset(decBoxTextRaw, 0, decBoxTextLen);
-      decBoxTextCleaned = decStrRaw;
-      if (decBoxTextCleaned.empty()) {
-        if (!hasNegativeSign) {
-          decBoxTextCleaned = "0";
-          strncpy_s(decBoxTextRaw, decBoxTextCleaned.c_str(), decBoxTextCleaned.size());
-        } else {
-          needToReplaceCleanedWithMinus = true;
-          decBoxTextCleaned = "0";
-          strncpy_s(decBoxTextRaw, "-", 1);
+        // Parse this string to an integer and convert it to decimal and binary
+        memset(decBoxTextRaw, 0, decBoxTextLen);
+        if (!hexStrRaw.empty()) {
+          std::string decimalStr = hexStrToDecimalStrForMode(hexStrRaw, decMode);
+          strncpy(decBoxTextRaw, decimalStr.c_str(), decimalStr.size());
+        }
+        memset(binBoxTextRaw, 0, binBoxTextLen);
+        if (!hexStrRaw.empty()) {
+          std::string binaryStr = hexStrToBinaryStr(hexStrRaw);
+          strncpy(binBoxTextRaw, binaryStr.c_str(), binaryStr.size());
         }
       } else {
-        strncpy_s(decBoxTextRaw, decBoxTextCleaned.c_str(), decBoxTextCleaned.size());
+        memset(hexBoxTextRaw, 0, hexBoxTextLen);
+        memset(decBoxTextRaw, 0, decBoxTextLen);
+        memset(binBoxTextRaw, 0, binBoxTextLen);
       }
-      // Parse this string to an integer and convert it to hex and binary
-      std::string hexStr = decStrToHexStr<uint32_t>(decBoxTextCleaned);
-      memset(hexBoxTextRaw, 0, hexBoxTextLen);
-      strncpy_s(hexBoxTextRaw, hexStr.c_str(), hexStr.size());
-      std::string binaryStr = decStrToBinaryStr<uint32_t>(decBoxTextCleaned);
-      memset(binBoxTextRaw, 0, binBoxTextLen);
-      strncpy_s(binBoxTextRaw, binaryStr.c_str(), binaryStr.size());
+    }
 
-      if (needToReplaceCleanedWithMinus) {
-        decBoxTextCleaned = "-";
+    if (decInputChanged) {
+      std::string decStrRaw{decBoxTextRaw};
+      if (decMode == DEC_MODE_U8 || decMode == DEC_MODE_U16 || decMode == DEC_MODE_U32 || decMode == DEC_MODE_U64) {
+        if (!decStrRaw.empty()) {
+          // Unsigned dec we just remove anything that isn't a decimal digit
+          decStrRaw.erase(
+              std::remove_if(decStrRaw.begin(), decStrRaw.end(), [](auto const& c) { return !decChars.contains(c); }));
+          // Update the value of the text boxes to reflect the cleaned value
+          memset(decBoxTextRaw, 0, decBoxTextLen);
+          if (!decStrRaw.empty()) {
+            strncpy(decBoxTextRaw, decStrRaw.c_str(), decStrRaw.size());
+          }
+          // Convert this value to decimal and binary
+          memset(hexBoxTextRaw, 0, hexBoxTextLen);
+          if (!decStrRaw.empty()) {
+            std::string hexStr = decStrToHexStrForMode(decStrRaw, decMode);
+            strncpy(hexBoxTextRaw, hexStr.c_str(), hexStr.size());
+          }
+          memset(binBoxTextRaw, 0, binBoxTextLen);
+          if (!decStrRaw.empty()) {
+            std::string binaryStr = decStrToBinaryStr(decStrRaw);
+            strncpy(binBoxTextRaw, binaryStr.c_str(), binaryStr.size());
+          }
+        } else {
+          memset(hexBoxTextRaw, 0, hexBoxTextLen);
+          memset(decBoxTextRaw, 0, decBoxTextLen);
+          memset(binBoxTextRaw, 0, binBoxTextLen);
+        }
+      } else {
+        bool hasOnlyMinus = (decStrRaw.size() == 1) && (decStrRaw.at(0) == '-');
+        if (!decStrRaw.empty() && !hasOnlyMinus) {
+          // Something has been typed into the decimal input, filter out bad characters
+          auto removeStart = decStrRaw.begin();
+          bool hasNegativeSign = false;
+          bool numberPortionEmpty = false;
+          bool needToReplaceCleanedWithMinus = false;
+          if (!decStrRaw.empty() && !decChars.contains(decStrRaw.at(0))) {
+            // Front of string doesn't have a number, remove it unless its a minus sign
+            if (!(decStrRaw.at(0) == '-')) {
+              decStrRaw.erase(0, 1);
+            } else {
+              hasNegativeSign = true;
+              ++removeStart;
+            }
+          }
+          decStrRaw.erase(
+              std::remove_if(removeStart, decStrRaw.end(), [](auto const& c) { return !decChars.contains(c); }));
+          // Update the value of the text boxes to reflect the cleaned value
+          memset(decBoxTextRaw, 0, decBoxTextLen);
+          if (decStrRaw.empty() || (decStrRaw.length() == 1 && hasNegativeSign)) {
+            numberPortionEmpty = true;
+            if (hasNegativeSign) {
+              needToReplaceCleanedWithMinus = true;
+              strncpy(decBoxTextRaw, "-", 1);
+            }
+          } else {
+            strncpy(decBoxTextRaw, decStrRaw.c_str(), decStrRaw.size());
+          }
+          // Parse this string to an integer and convert it to hex and binary
+          memset(hexBoxTextRaw, 0, hexBoxTextLen);
+          if (!numberPortionEmpty) {
+            std::string hexStr = decStrToHexStrForMode(decStrRaw, decMode);
+            strncpy(hexBoxTextRaw, hexStr.c_str(), hexStr.size());
+          }
+          memset(binBoxTextRaw, 0, binBoxTextLen);
+          if (!numberPortionEmpty) {
+            std::string binaryStr = decStrToBinaryStr(decStrRaw);
+            strncpy(binBoxTextRaw, binaryStr.c_str(), binaryStr.size());
+          }
+        } else {
+          if (!hasOnlyMinus) {
+            memset(decBoxTextRaw, 0, decBoxTextLen);
+          }
+          memset(hexBoxTextRaw, 0, hexBoxTextLen);
+          memset(binBoxTextRaw, 0, binBoxTextLen);
+        }
       }
     }
     //----------------------------------------------------------------------------------
@@ -228,12 +362,14 @@ int main(void) {
     GuiSetStyle(DEFAULT, TEXT_SIZE, 32);
     GuiLabel({16, 0, 280, 80}, hexLabelStr.c_str());
     GuiSetStyle(DEFAULT, TEXT_SIZE, 48);
+    std::string hexBoxTextOld{hexBoxTextRaw};
     if (GuiTextBox({16, 60, 280, 80}, hexBoxTextRaw, hexBoxTextLen, hexBoxEditMode)) {
       hexBoxEditMode = !hexBoxEditMode;
       if (hexBoxEditMode) {
         decBoxEditMode = false;
       }
     }
+    hexInputChanged = std::string{hexBoxTextRaw} != hexBoxTextOld;
     if (GuiButton({296, 60, 80, 80}, "#18#")) {
       // Copy via button
       SetClipboardText(hexBoxTextRaw);
@@ -242,25 +378,34 @@ int main(void) {
     GuiSetStyle(DEFAULT, TEXT_SIZE, 32);
     GuiLabel({16, 160, 280, 80}, decLabelStr.c_str());
     GuiSetStyle(DEFAULT, TEXT_SIZE, 48);
-    GuiTextBox({16, 220, 280, 80}, decBoxTextRaw, decBoxTextLen, decBoxEditMode);
-    // if (GuiTextBox({380, 60, 280, 80}, decBoxTextRaw, decBoxTextLen, decBoxEditMode)){
-    //   decBoxEditMode = !decBoxEditMode;
-    //   if (decBoxEditMode) {
-    //     hexBoxEditMode = false;
-    //   }
-    // }
+    std::string decBoxTextOld{decBoxTextRaw};
+    // GuiTextBox({16, 220, 280, 80}, decBoxTextRaw, decBoxTextLen, decBoxEditMode);
+    if (GuiTextBox({16, 220, 280, 80}, decBoxTextRaw, decBoxTextLen, decBoxEditMode)) {
+      decBoxEditMode = !decBoxEditMode;
+      if (decBoxEditMode) {
+        hexBoxEditMode = false;
+      }
+    }
+    decInputChanged = std::string{decBoxTextRaw} != decBoxTextOld;
     if (GuiButton({296, 220, 80, 80}, "#18#")) {
       // Copy via button
       SetClipboardText(decBoxTextRaw);
     }
 
     GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
-    GuiToggleGroup({420, 50, 60, 30}, "U8\nS8\nU16\nS16\nU32\nS32\nU64\nS64", &toggleGroup);
+    int newMode = decMode;
+    GuiToggleGroup({420, 50, 60, 30}, "U8\nS8\nU16\nS16\nU32\nS32\nU64\nS64", &newMode);
+    if (newMode != decMode) {
+      decModeToggled = true;
+      decMode = newMode;
+    }
 
     GuiSetStyle(DEFAULT, TEXT_SIZE, 32);
     GuiLabel({16, 320, 240, 80}, binLabelStr.c_str());
     GuiSetStyle(DEFAULT, TEXT_SIZE, 48);
+    std::string binBoxTextOld{binBoxTextRaw};
     GuiTextBox({16, 380, 480, 80}, binBoxTextRaw, binBoxTextLen, binBoxEditMode);
+    binInputChanged = std::string{binBoxTextRaw} != binBoxTextOld;
     if (GuiButton({496, 380, 80, 80}, "#18#")) {
       // Copy via button
       SetClipboardText(binBoxTextRaw);
